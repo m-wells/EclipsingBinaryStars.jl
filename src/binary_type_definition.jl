6e3454e129dc1917d373da3d895b7d14ed3946b3
m-wells <maw461@psu.edu>
2018-07-23 14:24:13 -0400

    fixing types

--- src/binary_type_definition.jl
+++ src/binary_type_definition.jl
@@ -25,11 +25,11 @@ end
 
 #---------------------------------------------------------------------------------------------------
 
-function getOrbit( ; ω = ω   :: Float64   # argument of periastron
-                   , ε = ε   :: Float64   # eccentricity
-                   , i = i   :: Float64   # inclination
-                   , a = a   :: Float64   # semi-major axis (in solar radii)
-                 )           :: Orbit
+function Orbit( ; ω = ω   :: Float64   # argument of periastron
+                , ε = ε   :: Float64   # eccentricity
+                , i = i   :: Float64   # inclination
+                , a = a   :: Float64   # semi-major axis (in solar radii)
+              )           :: Orbit
     return Orbit(ω, ε, i, a)
 end
 
@@ -44,9 +44,9 @@ end
 
 #---------------------------------------------------------------------------------------------------
 
-function getStar( ; m = m :: Float64  # mass
-                  , r = r :: Float64  # radius
-                )         :: Star
+function Star( ; m = m :: Float64  # mass
+               , r = r :: Float64  # radius
+             )         :: Star
     return Star(m, r)
 end
 
@@ -55,38 +55,38 @@ end
 #---------------------------------------------------------------------------------------------------
 
 """
-separation is in solar radii
+semi-major axis is in solar radii
 period is in days
 """
-function kepler3rdlaw_sep_to_per( ; pri = pri :: Star
-                                  , sec = sec :: Star
-                                  , orb = orb :: Orbit
-                                )             :: Float64
+function k3_a_to_P( pri :: Star
+                  , sec :: Star
+                  , orb :: Orbit
+                  )     :: Float64
     GM₁₂ = (pri.m + sec.m)*grav_msol_mks
-    per²  = (4π^2)*((orb.a*sol_radi_mks)^3)/GM₁₂
-    per = √(per²)
-    return per/secs_per_day
+    P²   = (4π^2)*((orb.a*sol_radi_mks)^3)/GM₁₂
+    P    = √(P²)
+    return P/secs_per_day
 end
 
 #---------------------------------------------------------------------------------------------------
 
 """
-separation is in solar radii
+semi-major axis is in solar radii
 period is in days
 """
-function kepler3rdlaw_per_to_sep( ; pri = pri :: Star
-                                  , sec = sec :: Star
-                                  , per = per :: Float64   # period (in days)
-                                  , ω = ω     :: Float64   # argument of periastron
-                                  , ε = ε     :: Float64   # eccentricity
-                                  , i = i     :: Float64   # inclination
-                                )             :: Float64
+function k3_P_to_a( pri   :: Star
+                  , sec   :: Star
+                  ; P = P :: Float64   # period (in days)
+                  , ω = ω :: Float64   # argument of periastron
+                  , ε = ε :: Float64   # eccentricity
+                  , i = i :: Float64   # inclination
+                  )       :: Float64
 
-    per² = (per*secs_per_day)^2
+    P²   = (P*secs_per_day)^2
     GM₁₂ = (pri.m + sec.m)*grav_msol_mks
-    sep³ = per²*GM₁₂/(4π^2)     # sep is the semi-major axis
-    sep  = ∛(sep³)
-    return sep/sol_radi_mks
+    a³ = P²*GM₁₂/(4π^2)     # a is the semi-major axis
+    a  = ∛(a³)
+    return a/sol_radi_mks
 end
 
 #---------------------------------------------------------------------------------------------------
@@ -97,38 +97,32 @@ struct Binary
     pri :: Star
     sec :: Star
     orb :: Orbit
-    per :: Float64
+    P   :: Float64
 end
 
 #---------------------------------------------------------------------------------------------------
 
-function getBinary( ; pri = pri :: Star
-                    , sec = sec :: Star
-                    , orb = orb :: Orbit
-                  )             :: Binary
+function Binary( pri :: Star
+               , sec :: Star
+               , orb :: Orbit
+               )     :: Binary
 
-    period = kepler3rdlaw_sep_to_per(pri=pri, sec=sec, orb=orb)
-    return Binary(pri, sec, orb, period)
+    P = k3_a_to_P(pri, sec, orb)
+    return Binary(pri, sec, orb, P)
 end
 
 #---------------------------------------------------------------------------------------------------
 
-function getBinary( ; pri = pri :: Star
-                    , sec = sec :: Star
-                    , per = per :: Float64     # period (in days)
-                    , ω   = ω   :: Float64     # argument of periastron
-                    , ε   = ε   :: Float64     # eccentricity
-                    , i   = i   :: Float64     # inclination
-                  )             :: Binary
+function Binary( pri                            :: Star
+               , sec                            :: Star
+               ; P   = error("missing keyword") :: Float64     # period (in days)
+               , ω   = error("missing keyword") :: Float64     # argument of periastron
+               , ε   = error("missing keyword") :: Float64     # eccentricity
+               , i   = error("missing keyword") :: Float64     # inclination
+               )                                :: Binary
     # get semi-major axis
-    a = kepler3rdlaw_per_to_sep( pri = pri
-                               , sec = sec
-                               , per = per
-                               , ω   = ω
-                               , ε   = ε
-                               , i   = i
-                               )
-    orb = Orbit(ω = ω, ε = ε, i = i, a = a)
+    a   = k3_P_to_a(pri, sec, P = P, ω = ω, ε = ε, i = i)
+    orb = Orbit(ω, ε, i, a)
 
-    return Binary(pri, sec, orb, per)
+    return Binary(pri, sec, orb, P)
 end
